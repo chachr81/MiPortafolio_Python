afb9b7bb1ffdd4b7c2a1c41908383ef45d774afd
Christian Chacón <christianangelchacon@hotmail.com>
2022-10-14 17:29:00 -0300

    nuevos ejercicios

---
+++ 30_dias_Python/ejerciciodia2.py
@@ -0,0 +1,56 @@
+# Day 2: 30 Days of python programming
+
+# Exercises: Level 1
+
+name = 'Christian'
+last_name = 'Chacón'
+full_name = 'Christian Chacón Romero'
+country = 'Venezuela'
+city = 'Santiago de Chile'
+age = '40'
+bday_year = '1981'
+is_married = 'yes'
+is_true = 'True'
+is_light = 'and Shine'
+first, last, bday = 'Christian', 'Chacón', 'December 30'
+
+# Exercise: Level 2
+
+print(type(name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(bday_year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+print(first, last, bday)
+print('first name length', len(name))
+print('last name length', len(last_name))
+
+num_one = 5
+num_two = 4
+variable_total = (num_one + num_two)
+variable_diff = (num_two - num_one)
+variable_product = (num_two * num_one)
+variable_division = (num_one / num_one)
+variable_reminder = (num_two / num_one)
+variable_exp = (num_one ** num_two)
+variable_FDvision = (num_one // num_two)
+
+print(variable_total, variable_diff, variable_product, variable_division, variable_reminder, variable_exp, variable_FDvision)
+
+# Calculating circle properties
+
+import math
+
+r = 30
+pi = math.pi
+print(pi)
+area_of_cirle = (pi * math.pow(r, 2))
+print(area_of_cirle)
+d = (2 * r)
+cir_of_circle = (pi * d)
+print(cir_of_circle)
