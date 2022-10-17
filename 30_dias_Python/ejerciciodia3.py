a56852203b824eb2b571f1b5c48b27172a395fd8
Christian Chacón <christianangelchacon@hotmail.com>
2022-10-17 16:45:28 -0300

    cambios dia 3

--- 30_dias_Python/ejerciciodia3.py
+++ 30_dias_Python/ejerciciodia3.py
@@ -2,3 +2,14 @@
 
 myAge = 40
 print('My age is: ', myAge)
+
+myHeight = 1.79
+print('My height is: ', myHeight)
+
+cnumber = 13j
+print(type(cnumber))
+
+h = int(input('Introduzca el valor de la altura: ', ))
+b = int(input('Introduzca el valor de la base: ', ))
+area = float(0.5 * b * h)
+print(area)
