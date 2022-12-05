6213e90791023335cbbad4ef11bfcdd7d4655deb
Christian Chacon <christianangelchacon@gmail.com>
2022-12-05 18:48:09 -0300

    cambios para prueba

--- pruebas_varias/example.py.py
+++
@@ -1,9 +0,0 @@
-numFeet = 30
-print('Este script convierte de pies a metros', '\n' + str(numFeet), 'pies es', str(numFeet * .3048), 'metros')
-
-mapDocumentName = 'Santiago'
-userInitials = 'CACR'
-print('New map document name', mapDocumentName+'_'+userInitials)
-
-for i in range(1,25):
-    print(i, '---sigue contando...')
